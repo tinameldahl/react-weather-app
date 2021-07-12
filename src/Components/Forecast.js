578196ee00a988fa674cb0a2eb53a547df4c1cc1
tinameldahl <tina@tinameldahl.no>
2021-07-12 08:47:38 +0200

    Changed forecast length

--- src/Components/Forecast.js
+++ src/Components/Forecast.js
@@ -24,7 +24,7 @@ function Forecast(props) {
       <div className="WeatherForecast">
         <div className="row">
           {forecast.map(function (dailyForecast, index) {
-            if (index < 5) {
+            if (index < 6) {
               return (
                 <div className="col" key={index}>
                   <WeatherForecastDay data={dailyForecast} />
